450f1ffc5aa6796580f2ff3b01c6e89466685310
LIKHITHL <lingutlalikithkumar@gmail.com>
2024-03-15 15:35:13 +0530

    privet commit

--- src/pages/admin/CreateCategory.js
+++ src/pages/admin/CreateCategory.js
@@ -20,7 +20,7 @@ const CreateCategory = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const { data } = await axios.post("${API_URL}/api/v1/category/create-category", {
+      const { data } = await axios.post(`${API_URL}/api/v1/category/create-category`, {
         name,
       });
       if (data?.success) {
