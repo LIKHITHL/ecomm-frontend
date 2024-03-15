c60175a1ed3dc15a5a64f6e054e7ff3599897cc9
LIKHITHL <lingutlalikithkumar@gmail.com>
2024-03-15 14:15:30 +0530

    ecomm

--- src/hooks/useCategory.js
+++ src/hooks/useCategory.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
-import {API_URL} from "../../config";
+import {API_URL} from "../config";
 
 
 export default function useCategory() {
